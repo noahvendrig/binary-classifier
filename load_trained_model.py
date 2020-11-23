4861a13d5cd3d7a839463c5fea2606d1060c7a69
sparkz11 <noahvendrig@gmail.com>
2020-11-24 00:44:06 +1100

    can now analyse all imgs in a directory

--- load_trained_model.py
+++ load_trained_model.py
@@ -1,10 +1,19 @@
 import cv2
+print(cv2.__version__)
+
+
 import tensorflow as tf
 import os
 
 os.chdir("D:/py/project/train_model")
 
 CATEGORIES = ['Dog', 'Not Dog']
+
+
+
+physical_devices = tf.config.list_physical_devices('GPU') 
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
 
 def prepare(filepath):
     IMG_SIZE = 50
@@ -14,23 +23,29 @@ def prepare(filepath):
 
 model = tf.keras.models.load_model("128x3-CNN.model")
 
-prediction = model.predict([prepare('dog.jpg')])
-print(prediction)
 
-# prediction = model.predict([prepare('dog.jpg')])
+prediction = model.predict([prepare('input_imgs/dog.jpg')]) # Initialising 'prediction' (doesn't actually matter what image is used here)
+#print(prediction)
+
+# prediction = model.predict([prepare('input_imgs/dog.jpg')])  # One by one approach, will only analyse images that have been selected
 # print(CATEGORIES[int(prediction[0][0])])
 
-# prediction = model.predict([prepare('cat.jpg')])
+# prediction = model.predict([prepare('input_imgs/cat.jpg')])
 # print(CATEGORIES[int(prediction[0][0])])
 
-# prediction = model.predict([prepare('bunny.jpg')])
+# prediction = model.predict([prepare('input_imgs/bunny.jpg')])
 # print(CATEGORIES[int(prediction[0][0])])
 
-imgs = ["dog.jpg", "cat.jpg", "bunny.jpg"]
+os.chdir("D:/py/project/train_model/input_imgs")
 
-def predict(imgs):
-    for img in imgs:
+filelist= []
+
+for filename in os.listdir("D:/py/project/train_model/input_imgs"): 
+    filelist.append(filename)
+
+def predict(filelist):
+    for img in filelist:
         prediction = model.predict([prepare(img)])
         print(img, "=",(CATEGORIES[int(prediction[0][0])]))
 
-predict(imgs)
+predict(filelist)
--- loading_data_tensorflow_tutorial_.py
+++ loading_data_tensorflow_tutorial_.py
@@ -141,9 +141,11 @@ conv_layers = [1, 2, 3]
 '''
 
 # From analysing TensorBoard data we can conclude that 3, 128, 2 gives us the best results
-dense_layers = [2]
-layer_sizes = [128]
+
+
 conv_layers = [3]
+layer_sizes = [128]
+dense_layers = [2]
 
 for dense_layer in dense_layers:
   for layer_size in layer_sizes:
