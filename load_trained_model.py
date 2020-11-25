76ccd9d34316f06f7de80c226e2e3f828938932b
sparkz11 <noahvendrig@gmail.com>
2020-11-26 00:59:52 +1100

    bad

--- load_trained_model.py
+++ load_trained_model.py
@@ -1,3 +1,4 @@
+'''
 # Import required modules
 
 import cv2
@@ -41,10 +42,9 @@ path = "D:/py/project/train_model/predictions"
 try:
     os.mkdir(path)  # Create the directory for prediction images
 except FileExistsError:
-    "Directory   %s already exists" % path
+    "Directory already exists"
 else:
-    print("Successfully created   %s ") % path
-
+    print("Successfully created "+ path)
 
 # os.chdir("D:/py/project/train_model/input_imgs")
 
@@ -56,12 +56,11 @@ for filename in os.listdir("D:/py/project/train_model/input_imgs"):
     filelist.append(filename) # Add each file in the directory to the list
 
 def predict(filelist):
-    for file in filelist:
-        prediction = model.predict([prepare(file)])
-        print(file, "=",(CATEGORIES[int(prediction[0][0])])) # Print prediction in the terminal
+    for image in filelist:
+        prediction = model.predict([prepare(image)])
+        print(image, "=",(CATEGORIES[int(prediction[0][0])])) # Print prediction in the terminal
 
-        25/11/2020
-        ####
+        '''
         str_prediction = (CATEGORIES[int(prediction[0][0])])
 
         img = cv2.imread(file)
@@ -72,8 +71,74 @@ def predict(filelist):
         #show img
         cv2.imshow(file, img) # Displays image in a window (easy for testing)
         cv2.waitKey(0)
-
+        '''
 
 predict(filelist) # Call prediction function
+'''
 
 
+
+
+
+
+
+import cv2
+print(cv2.__version__)
+
+
+import tensorflow as tf
+import os
+
+os.chdir("D:/py/project/train_model")
+
+CATEGORIES = ['Dog', 'Not Dog']
+
+
+
+physical_devices = tf.config.list_physical_devices('GPU') 
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+
+def prepare(filepath):
+    IMG_SIZE = 50
+    img_array = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
+
+def prepare(filepath):
+
+model = tf.keras.models.load_model("128x3-CNN.model")
+
+prediction = model.predict([prepare('dog.jpg')])
+print(prediction)
+
+# prediction = model.predict([prepare('dog.jpg')])
+prediction = model.predict([prepare('input_imgs/dog.jpg')]) # Initialising 'prediction' (doesn't actually matter what image is used here)
+#print(prediction)
+
+# prediction = model.predict([prepare('input_imgs/dog.jpg')])  # One by one approach, will only analyse images that have been selected
+# print(CATEGORIES[int(prediction[0][0])])
+
+# prediction = model.predict([prepare('cat.jpg')])
+# prediction = model.predict([prepare('input_imgs/cat.jpg')])
+# print(CATEGORIES[int(prediction[0][0])])
+
+# prediction = model.predict([prepare('bunny.jpg')])
+# prediction = model.predict([prepare('input_imgs/bunny.jpg')])
+# print(CATEGORIES[int(prediction[0][0])])
+
+imgs = ["dog.jpg", "cat.jpg", "bunny.jpg"]
+os.chdir("D:/py/project/train_model/input_imgs")
+
+filelist= []
+
+for filename in os.listdir("D:/py/project/train_model/input_imgs"): 
+    filelist.append(filename)
+
+def predict(imgs):
+    for img in imgs:
+def predict(filelist):
+    for img in filelist:
+        prediction = model.predict([prepare(img)])
+        print(img, "=",(CATEGORIES[int(prediction[0][0])]))
+
+predict(imgs)
+predict(filelist)
