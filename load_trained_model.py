7001b085e1e1678c7d5323ce2d6cef6b1fc8a8f2
sparkz11 <noahvendrig@gmail.com>
2020-11-26 23:37:38 +1100

    Draws prediction on img to output directory

--- load_trained_model.py
+++ load_trained_model.py
@@ -1,7 +1,4 @@
 import cv2
-print(cv2.__version__)
-
-
 import tensorflow as tf
 import os
 
@@ -18,7 +15,6 @@ print(filelist)
 
 physical_devices = tf.config.list_physical_devices('GPU') 
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
-
 
 def prepare(filepath):
     IMG_SIZE = 50
@@ -42,17 +38,30 @@ print(prediction)
 
 # prediction = model.predict([prepare('bunny.jpg')])
 # print(CATEGORIES[int(prediction[0][0])])
+output_path = "D:/py/project/train_model/output"
+
+try:
+    os.mkdir(output_path)
+except FileExistsError:
+    print("Directory already exists")
 
 os.chdir("D:/py/project/train_model/input_imgs")
 
-
+font = cv2.FONT_HERSHEY_SIMPLEX
 
 def predict(filelist):
     for img in filelist:
         prediction = model.predict([prepare(img)])
         print(img, "=",(CATEGORIES[int(prediction[0][0])]))
         
-
-
+        str_prediction = str(CATEGORIES[int(prediction[0][0])])
         
+        img_cv = cv2.imread(img)
+        img_cv = cv2.resize(img_cv, (1920,1080))
+
+        cv2.putText(img_cv, str_prediction, (200,200), font, 5, (255,0,0), 10)
+        #cv2.putText(img, "START", start_pt_centre, font, 3, (255, 0, 255), 3)
+
+        cv2.imwrite(output_path+'/'+img+'.jpg', img_cv)
+
 predict(filelist)
